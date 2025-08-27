df4c27557e6a48f068d0e9ad7d1e9686e909d24a
Leonardo Valdes <leon_valdes7591@yahoo.com>
2025-08-26 17:11:34 -0700

    complete console version with win and tie conditions

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function Gameboard() {
 
 	const setBoard = (mark, row, col) => {
 		if (board[row][col] === null) {
+			console.log(`player places ${mark} on row ${row}, column ${col}:`);
 			board[row][col] = mark;
 			return true;
 		} else {
@@ -18,6 +19,7 @@ function Gameboard() {
 	};
 
 	const printBoard = () => {
+		console.log('Current board:');
 		board.forEach((row) => {
 			console.log(row);
 		});
@@ -35,6 +37,7 @@ function createPlayer(playerMark) {
 function GameController() {
 	const board = Gameboard();
 	const players = [createPlayer('X'), createPlayer('O')];
+	const gameOn = true;
 
 	const initialPlayer = 0;
 	let activePlayer = players[initialPlayer];
@@ -45,19 +48,69 @@ function GameController() {
 
 	const getActivePlayer = () => activePlayer;
 
+	const hasWon = (player, row, col) => {
+		const playerMark = player.getMark();
+		const currentBoard = board.getBoard();
+
+		// check played row for win
+		const playedRow = currentBoard[row];
+		const isWinningRow = playedRow.every((cell) => cell === playerMark);
+
+		// check played column for win
+		let isWinningColumn = true;
+		for (let i = 0; i < currentBoard.length; i++) {
+			if (currentBoard[i][col] !== playerMark) {
+				isWinningColumn = false;
+			}
+		}
+
+		// check diagonals for win
+		const diagonal = [currentBoard[0][0], currentBoard[1][1], currentBoard[2][2]];
+		const antidiagonal = [currentBoard[0][2], currentBoard[1][1], currentBoard[2][0]];
+		const isWinningDiagonal =
+			diagonal.every((cell) => cell === playerMark) ||
+			antidiagonal.every((cell) => cell === playerMark);
+
+		if (isWinningRow || isWinningColumn || isWinningDiagonal) {
+			isWin = true;
+		}
+
+		return isWin;
+	};
+
+	const hasTied = () => {
+		const currentBoard = board.getBoard();
+		const remainingCells = [];
+		currentBoard.forEach((row) => {
+			row.forEach((cell) => {
+				if (cell === null) remainingCells.push(cell);
+			});
+		});
+		const isTie = remainingCells.length === 0 ? true : false;
+		return isTie;
+	};
+
 	const playTurn = (row, col) => {
 		const currentPlayer = getActivePlayer();
 		console.log(`${currentPlayer.getMark()}'s Turn...`);
 		// check for legal move
 		if (board.setBoard(currentPlayer.getMark(), row, col)) {
 			// check for winning patterns or ties
-
-			switchPlayer();
+			if (hasWon(currentPlayer, row, col)) {
+				// end game
+				console.log(`conditions are met for a win by ${currentPlayer.getMark()}`);
+			} else if (hasTied()) {
+				console.log('conditions are met for a tie');
+			} else {
+				console.log('no win or tie, continue playing...');
+				switchPlayer();
+			}
 		} else {
 			console.log('invalid move, try again.');
 		}
 		board.printBoard();
 	};
+
 	//initial render to console
 	board.printBoard();
 
@@ -65,3 +118,12 @@ function GameController() {
 }
 
 const game = GameController();
+game.playTurn(0, 0);
+game.playTurn(0, 2);
+game.playTurn(0, 1);
+game.playTurn(1, 0);
+game.playTurn(1, 2);
+game.playTurn(1, 1);
+game.playTurn(2, 0);
+game.playTurn(2, 2);
+game.playTurn(2, 1);
